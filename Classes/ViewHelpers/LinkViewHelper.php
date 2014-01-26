4c7ed76cdf44fe35386a1e24fec83f350e3ff17a
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-26 19:54:54 +0000

    initial commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_pwcomments_feed/trunk@83410 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/ViewHelpers/LinkViewHelper.php
@@ -0,0 +1,80 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2014 Jonathan Heilmann <mail@jonathan-heilmann.de>
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * ViewHelper to render links as the original but wit the option URIonly
+ *
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class Tx_JhPwcommentsFeed_ViewHelpers_LinkViewHelper extends Tx_Fluid_ViewHelpers_Link_PageViewHelper {
+
+	/**
+	 * @var string
+	 */
+	protected $tagName = 'a';
+
+	/**
+	 * Arguments initialization
+	 *
+	 * @return void
+	 */
+	public function initializeArguments() {
+		$this->registerUniversalTagAttributes();
+		$this->registerTagAttribute('target', 'string', 'Target of link', FALSE);
+		$this->registerTagAttribute('rel', 'string', 'Specifies the relationship between the current document and the linked document', FALSE);
+	}
+
+	/**
+	 * @param integer|NULL $pageUid target page. See TypoLink destination
+	 * @param array $additionalParams query parameters to be attached to the resulting URI
+	 * @param integer $pageType type of the target page. See typolink.parameter
+	 * @param boolean $noCache set this to disable caching for the target page. You should not need this.
+	 * @param boolean $noCacheHash set this to supress the cHash query parameter created by TypoLink. You should not need this.
+	 * @param string $section the anchor to be added to the URI
+	 * @param boolean $linkAccessRestrictedPages If set, links pointing to access restricted pages will still link to the page even though the page cannot be accessed.
+	 * @param boolean $absolute If set, the URI of the rendered link is absolute
+	 * @param boolean $addQueryString If set, the current query parameters will be kept in the URI
+	 * @param array $argumentsToBeExcludedFromQueryString arguments to be removed from the URI. Only active if $addQueryString = TRUE
+	 *	@param boolean $URIonly
+	 * @return string Rendered page URI
+	 */
+	public function render($pageUid = NULL, array $additionalParams = array(), $pageType = 0, $noCache = FALSE, $noCacheHash = FALSE, $section = '', $linkAccessRestrictedPages = FALSE, $absolute = FALSE, $addQueryString = FALSE, array $argumentsToBeExcludedFromQueryString = array(), $URIonly = FALSE) {
+		$uriBuilder = $this->controllerContext->getUriBuilder();
+		$uri = $uriBuilder->reset()->setTargetPageUid($pageUid)->setTargetPageType($pageType)->setNoCache($noCache)->setUseCacheHash(!$noCacheHash)->setSection($section)->setLinkAccessRestrictedPages($linkAccessRestrictedPages)->setArguments($additionalParams)->setCreateAbsoluteUri($absolute)->setAddQueryString($addQueryString)->setArgumentsToBeExcludedFromQueryString($argumentsToBeExcludedFromQueryString)->build();
+		if (strlen($uri) && !$URIonly) {
+			$this->tag->addAttribute('href', $uri);
+			$this->tag->setContent($this->renderChildren());
+			$result = $this->tag->render();
+		} else if (strlen($uri) && $URIonly) {
+			$result = $uri;
+		} else {
+			$result = $this->renderChildren();
+		}
+		return $result;
+	}
+}
+
+?>
